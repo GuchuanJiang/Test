a399391f272424a5a6c197afb9dbfb890acc9766
GuchuanJiang <jianggc1216@thundersoft.com>
2014-02-18 16:04:55 +0800

    add ant test project

---
+++ jTest/src/com/neusoft/test/AntTest.java
@@ -0,0 +1,12 @@
+
+package com.neusoft.test; 
+/** 
+ *This is just a test class. 
+*/ 
+public class AntTest{ 
+    public static void main(String[] args){ 
+        for(int i=0;i<args.length;i++){ 
+            System.out.println(args[i]); 
+        } 
+    } 
+} 
